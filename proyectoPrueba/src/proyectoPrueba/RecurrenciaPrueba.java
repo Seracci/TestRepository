81d214be07f9b46b4dcd627999d7bb3f59973196
Fer <Fer@DESKTOP-TM2PUHB>
2023-04-10 19:37:07 +0200

    Encontrar el menor recurrentemente

---
+++ proyectoPrueba/src/proyectoPrueba/RecurrenciaPrueba.java
@@ -0,0 +1,18 @@
+package proyectoPrueba;
+
+public class RecurrenciaPrueba {
+	public static int numeroMenor(int[] v, int numMenor, int i) {
+		if (i == v.length)
+			return numMenor;
+		
+		if (v[i] < numMenor)
+			numMenor = v[i];
+		return numeroMenor(v, numMenor, i+1);
+	}
+
+	public static void main(String[] args) {
+		int[] v = {5, 4, 3, 9};
+		System.out.println(numeroMenor(v, v[0], 0));
+	}
+
+}
