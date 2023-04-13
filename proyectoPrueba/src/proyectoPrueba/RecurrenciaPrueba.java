2d0aebf8e18ae4199ebd2196b18f906b19bdf48f
Seracci <130395361+Seracci@users.noreply.github.com>
2023-04-13 17:44:54 +0200

    Update RecurrenciaPrueba.java

--- proyectoPrueba/src/proyectoPrueba/RecurrenciaPrueba.java
+++ proyectoPrueba/src/proyectoPrueba/RecurrenciaPrueba.java
@@ -5,14 +5,7 @@ public class RecurrenciaPrueba {
 		if (i == v.length)
 			return numMenor;
 		
-		if (v[i] < numMenor)
-			numMenor = v[i];
-		return numeroMenor(v, numMenor, i+1);
-	}
-
-	public static void main(String[] args) {
-		int[] v = {5, 4, 3, 9};
-		System.out.println(numeroMenor(v, v[0], 1));
+		
 	}
 
 }
